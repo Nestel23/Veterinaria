e90af4757a79f4f702af990cc93d882cc68b0514
Nestel23 <126169013+Nestel23@users.noreply.github.com>
2023-03-01 15:10:11 -0500

    Create MascotasTest.php

---
+++ tests/Unit/MascotasTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class MascotasTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_mascotas(): void
+    {
+       $response = $this->get('mascotas');
+       $response->assertStatus(200);
+    }
+}
